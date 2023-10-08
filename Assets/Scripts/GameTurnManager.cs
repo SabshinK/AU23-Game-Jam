e0f9008911806ecc9b487777427be64a7ecf7097
Toby McGuire <70483097+tobymcguire0@users.noreply.github.com>
2023-10-07 21:36:35 -0400

    Dumb stupid error from last commit

--- Assets/Scripts/GameTurnManager.cs
+++ Assets/Scripts/GameTurnManager.cs
@@ -7,14 +7,13 @@ namespace King
     public class GameTurnManager : MonoBehaviour
     {
         //Replace GameObject with an interface which contains method "void StartTurn(GameTurnManager manager)"
-        List<GameObject> sentientObjects;
+        [SerializeField] List<GameObject> sentientObjects;
         [SerializeField] int turnCount = 30;
         public int GlobalTurnCount { get { return turnCount; } private set { turnCount = value; } }
         int objectTurn = 0;
 
         private void Start()
         {
-            sentientObjects = FindObjectsByType<ISentient>();
             //Start the first object's turn
             StartCoroutine(sentientObjects[objectTurn].GetComponent<ISentient>().StartTurn(this));
         }
