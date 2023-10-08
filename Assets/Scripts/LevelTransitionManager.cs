038868e9da280a19db253b8214ac8c1c477096f9
Toby McGuire <70483097+tobymcguire0@users.noreply.github.com>
2023-10-08 16:22:22 -0400

    LevelTransitions

--- Assets/Scripts/LevelTransitionManager.cs
+++ Assets/Scripts/LevelTransitionManager.cs
@@ -20,23 +20,57 @@ namespace King
             }
             Instance = this;
             DontDestroyOnLoad(this.gameObject);
+            TransitionIntoLevel();
         }
-
 
         bool transitioning = false;
         public void LoadLevel(string levelName)
         {
+            StartCoroutine(TransitionOutOfLevel(levelName));
         }
 
         private IEnumerator TransitionOutOfLevel(string levelName)
         {
-
+            //Force initial state to be a transparent screen
+            trasitionMat.SetFloat("_Opacity", 5);
+            trasitionMat.SetInt("_Inverse", 0);
+            transitioning = true;
+            //Load level in the background
             var async = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Single);
+            async.allowSceneActivation = false;
 
+            //Start the transition animation
+            LeanTween.value(trasitionMat.GetFloat("_Opacity"), 0, 0.5f).setOnUpdate((float f) =>
+            {
+                trasitionMat.SetFloat("_Opacity", f);
+            }).setOnComplete(
+                delegate ()
+                {
+                    transitioning = false;
+                });
+
+            //Wait until both the animation and loading are done
             while(!async.isDone && !transitioning)
             {
                 yield return null;
             }
+
+            //Allow the scene to transition
+            async.allowSceneActivation = true;
+
+
+            TransitionIntoLevel();
+        }
+        private void TransitionIntoLevel()
+        {
+            trasitionMat.SetInt("_Inverse", 1);
+            trasitionMat.SetFloat("_Opacity", 5);
+            LeanTween.value(trasitionMat.GetFloat("_Opacity"), 0, 0.5f).setOnUpdate((float f) =>
+            {
+                trasitionMat.SetFloat("_Opacity", f);
+            });
         }
     }
+
+    
 }
